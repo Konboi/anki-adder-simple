32eaaceee46f7711d0b2cc6d237e66c3a6a08a83
Konboi <ryosuke.yabuki@gmail.com>
2020-01-29 22:55:09 +0900

    add card component

---
+++ src/components/AddCard.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Container, Form, Header, TextArea, Button } from "semantic-ui-react";
+
+const AddCard = () => {
+  return (
+    <Container className="add-card">
+      <Container className="select-deck" style={{ paddingTop: 20 + "px" }}>
+        <Form>
+          <Form.Field
+            label="Current Deck"
+            control="select"
+            name="current-deck"
+          ></Form.Field>
+          <Form.Field
+            label="Note Type"
+            name="note-type"
+            control="select"
+          ></Form.Field>
+        </Form>
+      </Container>
+      <Container className="add-card-form" style={{ paddingTop: 20 + "px" }}>
+        <Header size="medium">Add Card</Header>
+        <Form>
+          <Form.Field inline>
+            <label>Front:</label>
+          </Form.Field>
+          <TextArea name="front" rows={2} />
+          <label>Back:</label>
+          <TextArea label="Back:" name="back" rows={2} />
+          <Form.Field label="Tags:" name="tags" control="input" />
+          <Form.Field inline>
+            <label style={{ paddingTop: 10 + "px" }}>
+              Connection Refused and no cached data.
+            </label>
+            <Button type="submit" positive floated="right">
+              Add Note
+            </Button>
+          </Form.Field>
+        </Form>
+      </Container>
+    </Container>
+  );
+};
+
+export default AddCard;
