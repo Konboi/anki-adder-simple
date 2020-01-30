b9fe636cb1753a7b2154a382eced47cd9d33f0a3
Konboi <ryosuke.yabuki@gmail.com>
2020-01-30 23:21:12 +0900

    add method to call version

---
+++ src/api/AnkiConnect.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+import config from "../config";
+
+const uri = `${config.defaultAnkiConnectUri}:${config.defaultAnkiConnectPort}`;
+
+const version = () => {
+  const req = {
+    action: "version",
+    version: config.supportAnkiConnectVersion
+  };
+
+  return axios.post(uri, req).then(result => {
+    if (result.data.error) {
+      throw new AnkiConnectAPIError(result.data.error);
+    }
+    return result.data.result;
+  });
+};
+
+class AnkiConnectAPIError {
+  constructor(message) {
+    this.message = message;
+    this.name = "AnkiConnectAPIError";
+  }
+}
+
+export default { version };
--- src/config.js
+++ src/config.js
@@ -1,4 +1,9 @@
-let defaultAnkiConnectUri = "http://127.0.0.0";
+let defaultAnkiConnectUri = "http://127.0.0.1";
 let defaultAnkiConnectPort = 8765;
+let supportAnkiConnectVersion = 6;
 
-export default { defaultAnkiConnectUri, defaultAnkiConnectPort };
+export default {
+  defaultAnkiConnectUri,
+  defaultAnkiConnectPort,
+  supportAnkiConnectVersion
+};
