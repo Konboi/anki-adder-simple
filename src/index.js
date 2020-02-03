4a43a3d881fcd9968a03c8e8cfaafd3a892c1eb4
Konboi <ryosuke.yabuki@gmail.com>
2020-02-03 21:16:35 +0900

    use module

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { MemoryRouter } from "react-router-dom";
+import "semantic-ui-css/semantic.min.css";
 
 ReactDOM.render(
   <MemoryRouter>
