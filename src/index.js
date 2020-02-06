1ff63f0262a3ce45560f064e30e885c529b58933
Konboi <ryosuke.yabuki@gmail.com>
2020-02-06 22:33:05 +0900

    add store

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { MemoryRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store/store";
 import "semantic-ui-css/semantic.min.css";
 
 ReactDOM.render(
   <MemoryRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </MemoryRouter>,
   document.getElementById("root")
 );
